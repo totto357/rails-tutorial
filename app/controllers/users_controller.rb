95001d3c3bdf060bdac0137752e2dde2bf00383a
totto357 <totto357@gmail.com>
2019-01-12 23:42:31 +0900

    feat(#11) ユーザ登録完了時にユーザ画面にリダイレクトするように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -11,16 +10,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
     else
-      render 'new'
+      render "new"
     end
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
