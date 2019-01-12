77210d8142335242ac72e57cd1ea62f2bc1fc5ec
totto357 <totto357@gmail.com>
2019-01-12 23:28:37 +0900

    fix(#11) ユーザ登録がgetになっていた問題の修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
-  get "/signup", to: "users#create"
+  post "/signup", to: "users#create"
 
   resources :users
 end
