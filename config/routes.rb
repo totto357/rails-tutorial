db1be35159f2ad1e25b4847c515cbd92ced98320
totto357 <totto357@gmail.com>
2019-01-16 23:19:01 +0900

    fix(#13) ログアウトをDELETEメソッドに変更

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # ログイン
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 
   # ユーザ情報
   resources :users
