e7312acfec218e8b96f04fc09d5fd02ba00e0b10
totto357 <totto357@gmail.com>
2019-01-17 18:27:00 +0900

    feat(#13) 文字列をハッシュ化するdigestをユーザモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true,
-                    length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+                    length: {maximum: 255},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
 
   before_save { email.downcase! }
 
   has_secure_password
+
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST : Bcrypt::Engine.cost
+    Bcrypt::Password.create(string, cost: cost)
+  end
 end
