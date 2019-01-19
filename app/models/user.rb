c58c6b602052a6bd3c29acb03099b6a058a425a4
totto357 <totto357@gmail.com>
2019-01-19 23:53:03 +0900

    feat(#15) セッション情報をユーザモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true,
                     length: {maximum: 255},
@@ -14,5 +16,16 @@ class User < ApplicationRecord
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザをDBに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
