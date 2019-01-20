39378b443ba34d204042215a3408e4adbceb8de1
totto357 <totto357@gmail.com>
2019-01-20 23:55:08 +0900

    fix(#15) ログアウト時の不具合を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ApplicationRecord
 
   # 渡されたトークンとダイジェストが一致しているかどうか
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,6 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
@@ -70,4 +69,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?("")
+  end
 end
