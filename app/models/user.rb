4cf45b29e52c071a35d2a15db7addb3753572c89
totto357 <totto357@gmail.com>
2019-01-20 23:19:07 +0900

    feat(#15) トークンを確認するメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,9 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # 渡されたトークンとダイジェストが一致しているかどうか
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
