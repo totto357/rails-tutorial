ea492b63d6164be8c9cef5534ee4b9a0ac4185a2
totto357 <totto357@gmail.com>
2019-01-25 18:55:35 +0900

    test(#17) ユーザ編集に関する認可のテストを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,8 +6,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
+
     patch user_path(@user),
           params: {
             user: {
@@ -17,12 +19,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
               password_confirmation: "bar",
             },
           }
+
     assert_template "users/edit"
     assert_select "div#error_explanation"
     assert_select "div.alert.alert-danger", "The form contains 4 errors."
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
 
