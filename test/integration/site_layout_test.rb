7d2670d8a962343a373267ef458b71eb0c0a6eac
totto357 <totto357@gmail.com>
2019-01-27 23:30:36 +0900

    test(#17) サイトレイアウトのテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,47 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
-  test "layout links" do
-    get root_path
-    assert_template "static_pages/home"
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-
-    get contact_path
-    assert_select "title", full_title("Contact")
+  def setup
+    @user = users(:michael)
   end
 
+  test "layout links without logged in" do
+    get root_path
+    assert_template "static_pages/home"
+
+    # ヘッダー
+    assert_select "header" do
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", login_path
+    end
+
+    # フッター
+    assert_select "footer" do
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+    end
+  end
+
+  test "layout links with logged in" do
+    log_in_as(@user)
+
+    get root_path
+    assert_template "static_pages/home"
+
+    # ヘッダー
+    assert_select "header" do
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", users_path
+      assert_select "a[href=?]", user_path(@user)
+      assert_select "a[href=?]", logout_path
+    end
+
+    # フッター
+    assert_select "footer" do
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+    end
+  end
 end
