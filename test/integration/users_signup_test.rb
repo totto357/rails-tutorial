a4aa37d3582f8a52bfe5df4d93707b546b10ec11
totto357 <totto357@gmail.com>
2019-01-30 19:46:45 +0900

    test(#19) アカウント有効化の統合テストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
@@ -19,7 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.alert.alert-danger"
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
       post users_path,
@@ -32,6 +36,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
              },
            }
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    # 有効化していない状態でログイン
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+
+    # トークンは正しいがメールアドレスが不正な場合
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+
+    # 正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
     follow_redirect!
     assert_template "users/show"
     assert_not flash.empty?
